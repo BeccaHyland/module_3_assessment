6fec4544548d34eb8fa9a28068cfeeb41c0d5369
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-30 09:56:37 -0600

    add spec test for search result sentences method

--- spec/models/sentence_search_result_spec.rb
+++ spec/models/sentence_search_result_spec.rb
@@ -6,4 +6,13 @@ describe SentenceSearchResult do
 
     expect(ssr).to be_a(SentenceSearchResult)
   end
+
+  context 'instance methods' do
+    it 'returns a collection of sentences' do
+      ssr = SentenceSearchResult.new("word")
+
+      expect(ssr.sentences).to be_a(Array)
+      expect(ssr.sentences.first).to be_a(Sentence)
+    end
+  end
 end
