e2a582caa67fa17ccf0608ef1fcafa733390b177
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-30 10:11:18 -0600

    add draft service methods

--- app/services/oxford_service.rb
+++ app/services/oxford_service.rb
@@ -2,4 +2,18 @@ class OxfordService
   def initialize(filter = {})
     @filter = filter
   end
+
+  def sentence_search
+    get_json("/api/v1/")
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: "https://od-api.oxforddictionaries.com")
+  end
+
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
 end
