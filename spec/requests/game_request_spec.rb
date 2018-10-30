0abb832fb1de437c5839a54c9218e7cceca4dcda
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-30 11:41:22 -0600

    adds route and controler for game api show

--- spec/requests/game_request_spec.rb
+++ spec/requests/game_request_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe "Games API" do
   it "can get one game by its id" do
+    # When I send a GET request to “/api/v1/games/1” I receive a JSON response as follows:
     id = create(:game).id
 
-    get "/api/v1/game#{id}"
+    get "/api/v1/games/#{id}"
 
     g = JSON.parse(response.body)
 
